05c516e0372e0b9a2c87313ffdf5650fccccdb7c
annammattis <54290779+annammattis@users.noreply.github.com>
2019-11-13 18:53:22 -0500

    update 2

--- game.js
+++ game.js
@@ -4,26 +4,25 @@ var randomWord;
 //create variables for wins, losses, # of guesses, letters guessed, reset
 var wins = 0;
 var losses = 0;
-var numOfGuesses = 15;
-var lettersGuessed = [];
+var numOfGuesses;
+var displayWord = [];
 
 function gameSetUp() {
   numOfGuesses = 15;
   randomWord = [Math.floor(Math.random() * words.length)];
+  for (var i = 0; i < randomWord.length; i++) {
+    displayWord.push('_');
+  }
+  console.log('randomWord', randomWord);
+  console.log('displayWord', displayWord);
+
+  document.getElementById('guessedWord').innerHTML = displayWord.join(' ');
 }
 
-function resetGame() {
-  numOfGuesses = 15;
-  lettersGuessed = [];
-}
-
-//define keypress
-document.onkeypress = function(event) {
-
-}
-
-//Create a for-loop to iterate through the words array
-for (var i = 0; i = < words.length; i++) {
-
-}
-}
+// 1. Pick a random word.
+// 2. Take the player’s guess.
+// 3. Quit the game if the player wants to.
+// 4. Check that the player’s guess is a valid letter.
+// 5. Keep track of letters the player has guessed.
+// 6. Show the player their progress.
+// 7. Finish when the player has guessed the word.
